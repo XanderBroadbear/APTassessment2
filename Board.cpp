db44e41af7c9ce3b81f87c5965cc889d611392f7
XANDER BROADBEAR <s3902024@student.rmit.edu.au>
2022-05-05 10:45:51 +1000

    started printBoard()

--- Board.cpp
+++ Board.cpp
@@ -8,9 +8,14 @@
 
 Board::Board(){
     for(int i = 0; i <= BOARD_SIZE; i++){
-        std::cout << "\n" << std::endl;
         for(int j = 0; j <= BOARD_SIZE; j++){
             Tile tile = Tile(' ', 0, i, j);
         }
     }
+
+Board::printBoard(){
+    std::cout << "\n" << std::endl;
+    std::cout << tile.letter << std::endl;
+
+}
 }
